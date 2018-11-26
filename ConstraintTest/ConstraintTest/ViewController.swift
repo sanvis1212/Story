7441591e277e6b7c35ba9710fceee16df76bae81
Vipin Pachauri <appther@Vipins-Mac-mini.local>
2018-11-26 11:19:45 +0530

    second commit

---
+++ ConstraintTest/ConstraintTest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ConstraintTest
+//
+//  Created by Vipin Pachauri on 26/11/18.
+//  Copyright © 2018 ApptherMobilityTechnologiesPrivateLimited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setUpView()
+
+    }
+    
+    private func setUpView(){
+   
+    }
+
+
+}
