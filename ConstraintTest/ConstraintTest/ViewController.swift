1e46b08caf378601a107a1739276dfac3e6fecda
Vipin Pachauri <appther@Vipins-Mac-mini.local>
2019-01-14 10:58:56 +0530

    pod file changes

--- ConstraintTest/ConstraintTest/ViewController.swift
+++ ConstraintTest/ConstraintTest/ViewController.swift
@@ -9,46 +9,70 @@
 import UIKit
 import Foundation
 
+
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.dataRecieveFromServer()
+//        self.dataRecieveFromServer()
         
     }
+    
+    
     
     fileprivate func dataRecieveFromServer(){
-        
-        let url = "https://api.letsbuildthatapp.com/jsondecodable/course"
-        guard let urlString = URL(string: url) else {
-            print("Url not found ")
+        let urlString = "https://api.letsbuildthatapp.com/jsondecodable/course"
+        guard let url = URL(string: urlString) else {
+            print("Url is not valid")
             return
         }
-        var requeset = URLRequest(url: urlString)
-        requeset.httpMethod = "GET"
-    
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
         
-         URLSession.shared.dataTask(with: requeset) { (data, response, error) in
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard error == nil else {
-                print("Din't get response")
-                return
+                return print("Dint get response")
             }
             guard let responseData = data else {
-                print("Data not found")
+                print("response not found")
                 return
             }
-            
-            do {
-                let objectData = try JSONSerialization.jsonObject(with: responseData, options: [])
-                
-                print(objectData)
-            } catch {
-                print("error found")
-            }
-            
-        }.resume()
-        
+            //PArsing is pending
+        }
     }
+    
+//    fileprivate func dataRecieveFromServer(){
+//
+//        let url = "https://api.letsbuildthatapp.com/jsondecodable/course"
+//        guard let urlString = URL(string: url) else {
+//            print("Url not found ")
+//            return
+//        }
+//        var requeset = URLRequest(url: urlString)
+//        requeset.httpMethod = "GET"
+//
+//
+//         URLSession.shared.dataTask(with: requeset) { (data, response, error) in
+//            guard error == nil else {
+//                print("Din't get response")
+//                return
+//            }
+//            guard let responseData = data else {
+//                print("Data not found")
+//                return
+//            }
+//
+//            do {
+//                let objectData = try JSONSerialization.jsonObject(with: responseData, options: [])
+//
+//                print(objectData)
+//            } catch {
+//                print("error found")
+//            }
+//
+//        }.resume()
+//
+//    }
     
 }
 
